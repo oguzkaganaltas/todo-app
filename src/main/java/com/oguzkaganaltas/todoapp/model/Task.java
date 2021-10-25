441a64f0ac9959cea7ff81265f83c31c313ebac8
oguz <oguzkaganaltas@gmail.com>
2021-10-25 20:33:31 +0300

    task class created

---
+++ src/main/java/com/oguzkaganaltas/todoapp/model/Task.java
@@ -0,0 +1,54 @@
+package com.oguzkaganaltas.todoapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+    private int id;
+    private String title;
+    private String note;
+    private boolean status;
+
+    public Task(int id, String title, String note, boolean status) {
+        this.id = id;
+        this.title = title;
+        this.note = note;
+        this.status = status;
+    }
+
+    @Id
+    @GeneratedValue
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
