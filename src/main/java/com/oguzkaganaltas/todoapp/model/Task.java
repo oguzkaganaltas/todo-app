63392fe6f6f200248ed50910fd01ff03a17a2812
oguz <oguzkaganaltas@gmail.com>
2021-10-26 12:40:32 +0300

    lombok used for clean code. DAte added to Task object

--- src/main/java/com/oguzkaganaltas/todoapp/model/Task.java
+++ src/main/java/com/oguzkaganaltas/todoapp/model/Task.java
@@ -1,56 +1,26 @@
 package com.oguzkaganaltas.todoapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Task {
 
     @Id
-    @GeneratedValue
     private int id;
     private String title;
     private String note;
+    private Date date = new Date();
     private boolean status;
 
-    public Task(int id, String title, String note, boolean status) {
-        this.id = id;
-        this.title = title;
-        this.note = note;
-        this.status = status;
-    }
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public boolean isStatus() {
-        return status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
 }
