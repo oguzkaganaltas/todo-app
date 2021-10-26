db401943b8a724dd8cdaeac1e9dedf66572e4b14
oguz <oguzkaganaltas@gmail.com>
2021-10-26 12:42:54 +0300

    taskService used to clean the software structure

--- src/main/java/com/oguzkaganaltas/todoapp/controller/TaskController.java
+++ src/main/java/com/oguzkaganaltas/todoapp/controller/TaskController.java
@@ -1,42 +1,56 @@
 package com.oguzkaganaltas.todoapp.controller;
 
 import com.oguzkaganaltas.todoapp.model.Task;
-import org.springframework.http.HttpStatus;
+import com.oguzkaganaltas.todoapp.service.TaskService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/tasks")
+@AllArgsConstructor
 public class TaskController {
 
-    private final List<Task> tasks;
-
-    public TaskController() {
-        Task task1 = new Task(1,"task1","hello",false);
-        Task task2 = new Task(2,"task2","world",false);
-
-        tasks = Arrays.asList(task1,task2);
-    }
+    private final TaskService taskService;
 
     @GetMapping
     public ResponseEntity<List<Task>> getTasks(){
-        return new ResponseEntity<>(tasks, OK);
+        return new ResponseEntity<>(taskService.getAllTasks(), OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTask(@PathVariable int id){
-        Task result = this.tasks.stream().filter(task -> task.getId() == id)
-                .findFirst()
-                .orElseThrow(() ->new RuntimeException("Task not found"));
-        return new ResponseEntity<>(result, OK);
+        return new ResponseEntity<>(getResult(id), OK);
     }
 
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody Task newTask){
+        return new ResponseEntity<>(taskService.createTask(newTask), CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> getTask(@PathVariable int id, @RequestBody Task newTask){
+        Task oldTask = getResult(id);
+        Objects.requireNonNull(oldTask).setTitle(newTask.getTitle());
+        oldTask.setNote(newTask.getNote());
+        oldTask.setStatus(newTask.isStatus());
+
+        return new ResponseEntity<>(OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable int id){
+        taskService.deleteTask(id);
+        return new ResponseEntity<>(OK);
+    }
+
+    private Task getResult(int id) {
+        return taskService.getTaskById(id);
+    }
 }
