493be419e7fd7372ce78d464c15bfd6ed5c7d69a
oguz <oguzkaganaltas@gmail.com>
2021-10-26 19:43:42 +0300

    code snippets removed and lombok used for getter setters and constructor

--- src/main/java/com/oguzkaganaltas/todoapp/model/User.java
+++ src/main/java/com/oguzkaganaltas/todoapp/model/User.java
@@ -1,52 +1,24 @@
 package com.oguzkaganaltas.todoapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.ArrayList;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
-
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
     private String username;
     private String password;
-    private final ArrayList<Project> projects;
-
-    public User(int id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.projects = new ArrayList<>();
-    }
-
-    public User() {
-        this.projects = new ArrayList<>();
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
