894812c7a7919bbff7a2e3efecf95a27c25c787c
oguz <oguzkaganaltas@gmail.com>
2021-10-27 10:51:50 +0300

    User repository created for db connection

---
+++ src/main/java/com/oguzkaganaltas/todoapp/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.oguzkaganaltas.todoapp.repository;
+
+import com.oguzkaganaltas.todoapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+}
