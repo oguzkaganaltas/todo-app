acd5e3a3672cb1d7aef7d11ad3267367af0d1ba5
oguz <oguzkaganaltas@gmail.com>
2021-10-30 12:55:06 +0300

    http status static variables import updated

--- src/main/java/com/oguzkaganaltas/todoapp/controller/TaskController.java
+++ src/main/java/com/oguzkaganaltas/todoapp/controller/TaskController.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("/tasks")
