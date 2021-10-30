aee6dd4f873638a9dcb3092c320c6259dd0c46e6
oguz <oguzkaganaltas@gmail.com>
2021-10-30 18:49:12 +0300

    session id generator implemented

--- src/main/java/com/oguzkaganaltas/todoapp/controller/UserController.java
+++ src/main/java/com/oguzkaganaltas/todoapp/controller/UserController.java
@@ -32,6 +32,15 @@ public class UserController {
         return userService.getAllUsers().stream().anyMatch(obj -> Objects.equals(obj.getEmail(), newUser.getEmail()));
     }
 
+    private static final SecureRandom secureRandom = new SecureRandom(); //threadsafe
+    private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder(); //threadsafe
+
+    public static String generateNewToken() {
+        byte[] randomBytes = new byte[24];
+        secureRandom.nextBytes(randomBytes);
+        return base64Encoder.encodeToString(randomBytes);
+    }
+
     @PostMapping("/login")
     public ResponseEntity<User> loginUser(@RequestBody User user){
         List<User> users = userService.getAllUsers();
