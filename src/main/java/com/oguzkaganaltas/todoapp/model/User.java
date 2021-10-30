629f69bc23c3220948204c74446f64acb376d7e7
oguz <oguzkaganaltas@gmail.com>
2021-10-30 18:43:39 +0300

    password hash size increased to 512

--- src/main/java/com/oguzkaganaltas/todoapp/model/User.java
+++ src/main/java/com/oguzkaganaltas/todoapp/model/User.java
@@ -23,7 +23,7 @@ public class User {
     @Column(nullable = false, unique = true, length = 45)
     private String email;
 
-    @Column(nullable = false, length = 64)
+    @Column(nullable = false, length = 512)
     private String password;
 
     @Column(name = "first_name", nullable = false, length = 20)
