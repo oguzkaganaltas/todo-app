9ebbebba0ec27ff0e6c25a27bfc2283e6a2f2835
oguz <oguzkaganaltas@gmail.com>
2021-11-01 11:16:50 +0200

    endpoint namings fixed, sessionId added to methods

--- src/main/java/com/oguzkaganaltas/todoapp/controller/ProjectController.java
+++ src/main/java/com/oguzkaganaltas/todoapp/controller/ProjectController.java
@@ -1,10 +1,12 @@
 package com.oguzkaganaltas.todoapp.controller;
 
 import com.oguzkaganaltas.todoapp.model.Project;
-import com.oguzkaganaltas.todoapp.model.Task;
+import com.oguzkaganaltas.todoapp.model.User;
 import com.oguzkaganaltas.todoapp.service.ProjectService;
+import com.oguzkaganaltas.todoapp.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,15 +18,18 @@ import static org.springframework.http.HttpStatus.*;
 @AllArgsConstructor
 public class ProjectController {
     private final ProjectService projectService;
+    private final UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<Project>> getProjects(){
-        return new ResponseEntity<>(this.projectService.getAllProjects(), OK);
+    public ResponseEntity<List<Project>> getProjects(@RequestHeader("sessionId") String sessionId){
+        return new ResponseEntity<>(this.projectService.getProjectsByOwnerId(userService.getUserBySessionId(sessionId).getId()), OK);
     }
 
-    @GetMapping("/{project_id}")
-    public ResponseEntity<Project> getProject(@PathVariable int project_id){
-        return new ResponseEntity<>(this.projectService.getProjectById(project_id),OK);
+    @GetMapping("/{projectId}")
+    public ResponseEntity<Project> getProject(@PathVariable int projectId,@RequestHeader("sessionId") String sessionId){
+        User user = userService.getUserBySessionId(sessionId);
+        Project project = projectService.getProjectByOwnerId(user.getId(),projectId);
+        return new ResponseEntity<>(project,OK);
     }
 
     private boolean isAnyMatch(Project newProject) {
@@ -32,27 +37,28 @@ public class ProjectController {
     }
 
     @PostMapping("/new-project")
-    public ResponseEntity<Project> createProject(@RequestBody Project newProject){
+    public ResponseEntity<Project> createProject(@RequestBody Project newProject, @RequestHeader("sessionId") String sessionId){
+        User user = userService.getUserBySessionId(sessionId);
+        newProject.setOwnerId(user.getId());
         if (isAnyMatch(newProject)){
             return new ResponseEntity<>(CONFLICT);
         }
         return new ResponseEntity<>(this.projectService.createProject(newProject), CREATED);
     }
 
-    @PutMapping("/update/{project_id}")
-    public ResponseEntity<Project> updateProject(@PathVariable int project_id, @RequestBody Project newProject){
-        Project oldProject = getResult(project_id);
+    @PutMapping("/update/{projectId}")
+    public ResponseEntity<Project> updateProject(@PathVariable int projectId, @RequestBody Project newProject,@RequestHeader("sessionId") String sessionId){
+        User user = userService.getUserBySessionId(sessionId);
+        Project oldProject = projectService.getProjectByOwnerId(user.getId(), projectId);
         oldProject.setName(newProject.getName());
         return new ResponseEntity<>(projectService.createProject(oldProject),OK);
     }
-
-    @DeleteMapping("/remove/{project_id}")
-    public ResponseEntity<Void> deleteTask(@PathVariable int project_id){
-        this.projectService.deleteProject(project_id);
+    @Transactional
+    @DeleteMapping("/remove/{projectId}")
+    public ResponseEntity<Void> deleteTask(@PathVariable int projectId, @RequestHeader("sessionId") String sessionId){
+        User user = userService.getUserBySessionId(sessionId);
+        this.projectService.deleteProject(projectId, user.getId());
         return new ResponseEntity<>(OK);
     }
 
-    private Project getResult(int id) {
-        return this.projectService.getProjectById(id);
-    }
 }
