5e77cf20b5c6a70ca1e53aaeace5932d878982d6
oguz <oguzkaganaltas@gmail.com>
2021-11-01 17:05:25 +0200

    errors handled with try catch

--- src/main/java/com/oguzkaganaltas/todoapp/controller/ProjectController.java
+++ src/main/java/com/oguzkaganaltas/todoapp/controller/ProjectController.java
@@ -22,41 +22,77 @@ public class ProjectController {
 
     @GetMapping
     public ResponseEntity<List<Project>> getProjects(@RequestHeader("sessionId") String sessionId){
-        return new ResponseEntity<>(this.projectService.getProjectsByOwnerId(userService.getUserBySessionId(sessionId).getId()), OK);
+        User user;
+        try{
+            user = userService.getUserBySessionId(sessionId);
+        }catch (RuntimeException exception){
+            return new ResponseEntity<>(NOT_ACCEPTABLE);
+        }
+
+        List<Project> projects;
+        try {
+            projects = projectService.getProjectsByOwnerId(user.getId());
+        }catch (RuntimeException exception){
+            return new ResponseEntity<>(NOT_FOUND);
+        }
+        return new ResponseEntity<>(projects, OK);
     }
 
     @GetMapping("/{projectId}")
     public ResponseEntity<Project> getProject(@PathVariable int projectId,@RequestHeader("sessionId") String sessionId){
-        User user = userService.getUserBySessionId(sessionId);
-        Project project = projectService.getProjectByOwnerId(user.getId(),projectId);
+        User user;
+        try{
+            user = userService.getUserBySessionId(sessionId);
+        }catch (RuntimeException exception){
+            return new ResponseEntity<>(NOT_ACCEPTABLE);
+        }
+        Project project;
+        try {
+            project = projectService.getProjectByOwnerId(user.getId(),projectId);
+        }catch (RuntimeException exception){
+            return new ResponseEntity<>(NOT_FOUND);
+        }
         return new ResponseEntity<>(project,OK);
-    }
-
-    private boolean isAnyMatch(Project newProject) {
-        return projectService.getAllProjects().stream().anyMatch(obj -> obj.getId() == newProject.getId());
     }
 
     @PostMapping("/new-project")
     public ResponseEntity<Project> createProject(@RequestBody Project newProject, @RequestHeader("sessionId") String sessionId){
-        User user = userService.getUserBySessionId(sessionId);
-        newProject.setOwnerId(user.getId());
-        if (isAnyMatch(newProject)){
-            return new ResponseEntity<>(CONFLICT);
+        User user;
+        try {
+            user = userService.getUserBySessionId(sessionId);
+        }catch (RuntimeException exception){
+            return new ResponseEntity<>(NOT_ACCEPTABLE);
         }
+        newProject.setOwnerId(user.getId());
         return new ResponseEntity<>(this.projectService.createProject(newProject), CREATED);
     }
 
     @PutMapping("/update/{projectId}")
     public ResponseEntity<Project> updateProject(@PathVariable int projectId, @RequestBody Project newProject,@RequestHeader("sessionId") String sessionId){
-        User user = userService.getUserBySessionId(sessionId);
-        Project oldProject = projectService.getProjectByOwnerId(user.getId(), projectId);
+        User user;
+        try{
+            user = userService.getUserBySessionId(sessionId);
+        }catch (RuntimeException exception){
+            return new ResponseEntity<>(NOT_ACCEPTABLE);
+        }
+        Project oldProject;
+        try {
+            oldProject = projectService.getProjectByOwnerId(user.getId(),projectId);
+        }catch (RuntimeException exception){
+            return new ResponseEntity<>(NOT_FOUND);
+        }
         oldProject.setName(newProject.getName());
         return new ResponseEntity<>(projectService.createProject(oldProject),OK);
     }
     @Transactional
     @DeleteMapping("/remove/{projectId}")
     public ResponseEntity<Void> deleteTask(@PathVariable int projectId, @RequestHeader("sessionId") String sessionId){
-        User user = userService.getUserBySessionId(sessionId);
+        User user;
+        try{
+            user = userService.getUserBySessionId(sessionId);
+        }catch (RuntimeException exception){
+            return new ResponseEntity<>(NOT_ACCEPTABLE);
+        }
         this.projectService.deleteProject(projectId, user.getId());
         return new ResponseEntity<>(OK);
     }
