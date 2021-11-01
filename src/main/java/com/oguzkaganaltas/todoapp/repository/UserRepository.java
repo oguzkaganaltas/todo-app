06c19569770e5d999cd4c4df7eba881bc770a0f7
oguz <oguzkaganaltas@gmail.com>
2021-11-01 11:15:48 +0200

    modified queries for session id added

--- src/main/java/com/oguzkaganaltas/todoapp/repository/UserRepository.java
+++ src/main/java/com/oguzkaganaltas/todoapp/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.oguzkaganaltas.todoapp.repository;
 
 import com.oguzkaganaltas.todoapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
-
+    @Query(value = "SELECT * FROM USERS WHERE SESSION_ID = ?1",nativeQuery = true)
+    User findUserBySessionId(String sessionId);
 }
