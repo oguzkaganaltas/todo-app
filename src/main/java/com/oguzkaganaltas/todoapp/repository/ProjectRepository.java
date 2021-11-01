ff9ce7dffb2bbfc6829b6593fe3457ce7c2a5e6b
oguz <oguzkaganaltas@gmail.com>
2021-11-01 11:15:05 +0200

    modified queries for session id added

--- src/main/java/com/oguzkaganaltas/todoapp/repository/ProjectRepository.java
+++ src/main/java/com/oguzkaganaltas/todoapp/repository/ProjectRepository.java
@@ -9,4 +9,13 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
+    @Query(value = "SELECT * FROM PROJECTS WHERE OWNER_ID = ?1",nativeQuery = true)
+    List<Project> findProjectsByOwnerId(long ownerId);
+    @Query(value = "SELECT * FROM PROJECTS WHERE OWNER_ID = ?1 AND PROJECT_ID = ?2",nativeQuery = true)
+    Project findProjectByOwnerId(long ownerId, int projectId);
+
+    @Modifying
+    @Query(value = "DELETE FROM PROJECTS p WHERE p.OWNER_ID =:ownerId AND p.PROJECT_ID =:projectId",nativeQuery = true)
+    void deleteProjectById(@Param("projectId") int projectId,@Param("ownerId") long ownerId);
+
 }
