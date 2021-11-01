096d4c3ce6b43e9058077927e062cb2cf3a58a3c
oguz <oguzkaganaltas@gmail.com>
2021-11-01 11:14:46 +0200

    import

--- src/main/java/com/oguzkaganaltas/todoapp/repository/ProjectRepository.java
+++ src/main/java/com/oguzkaganaltas/todoapp/repository/ProjectRepository.java
@@ -2,6 +2,11 @@ package com.oguzkaganaltas.todoapp.repository;
 
 import com.oguzkaganaltas.todoapp.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
 }
